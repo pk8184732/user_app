e2207faac9842aa43214654f80229d3f6c0e2f42
Puja <154424517+pk8184732@users.noreply.github.com>
2025-02-06 13:32:22 +0530

    we create a like screen

--- lib/views/user_favorite/user_favorite_screen.dart
+++ lib/views/user_favorite/user_favorite_screen.dart
@@ -78,9 +78,11 @@
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
+
 class LikedProductsScreen extends StatefulWidget {
   final List<String> likedProductIds;
   final Function(String) toggleLike;
+
 
   LikedProductsScreen({required this.likedProductIds, required this.toggleLike});
 
